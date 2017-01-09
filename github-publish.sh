b9c1160fcc7b94442a017bdd934f1fd7d832fd4e
Oliver Trosien <otrosien@epages.com>
2017-01-09 15:21:58 +0100

    Add binary tool: github-release CLI

--- github-publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e # Exit with nonzero exit code if anything fails
-
-if [[ "$TRAVIS_TAG" =~ ^[0-9.]+$ ]] ; then
-	tools/github-release release --security-token "$GITHUB_TOKEN" --user ePages-de --repo yaml-merge --tag "$TRAVIS_TAG" --description "Release $TRAVIS_TAG"
-	tools/github-release upload --security-token "$GITHUB_TOKEN" --user ePages-de --repo yaml-merge --tag "$TRAVIS_TAG" --name yaml-merge-${TRAVIS_TAG}.zip --file build/distributions/yaml-merge-${TRAVIS_TAG}.zip
-fi
